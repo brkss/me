b04004a9b84bb664d12045b9322290e6aba1e007
brkss <berkassebrahim@gmail.com>
2021-09-11 03:47:24 +0100

    📦 INIT

---
+++ portfolio/src/utils/config/route.ts
@@ -0,0 +1,10 @@
+import { IRoute } from "../types/Route";
+
+export const routes: IRoute[] = [
+  {
+    name: "Home Page",
+    path: "/",
+    exact: true,
+    component: null,
+  },
+];
---
+++ portfolio/src/utils/types/Route.ts
@@ -0,0 +1,6 @@
+export interface IRoute {
+  name: string;
+  path: string;
+  component: any;
+  exact: boolean;
+}
