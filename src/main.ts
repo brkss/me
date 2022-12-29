699992f16efc6b496cc5ae16fa70f528005ced04
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-29 21:20:41 +0100

    🔨 Home styling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-	<h1>Hello !</h1>  
+	<div class='main'>
+		<h1 class='nav-title'>ABOUT</h1>  
+		<h1 class='nav-title right-text'>CONTACT</h1>  
+		<h1 class='nav-title'>PROJECTS</h1>  
+	</div>
 `
 
