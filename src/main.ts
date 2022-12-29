ad4eb7df25b4ec4a6b5b0ab4972c6c36c5698944
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-29 21:35:43 +0100

    🍕 add hammer !

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-	<div class='main'>
-		<h1 class='nav-title'>ABOUT</h1>  
-		<h1 class='nav-title right-text'>CONTACT</h1>  
-		<h1 class='nav-title'>PROJECTS</h1>  
+	<div class='center'>
+		<div class='main'>
+			<h1 class='nav-title'>ABOUT</h1>  
+			<h1 class='nav-title right-text'>CONTACT</h1>  
+			<h1 class='nav-title'>PROJECTS</h1>  
+		</div>
 	</div>
 `
 
