9b5485a33dbb177a87f3e68e2811fecb80905457
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-30 23:51:23 +0100

    general track components !

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import './style.css'
+import { showTrack } from './track';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-	<div class='center'>
-		<div class='main'>
-			<h1 class='nav-title'>ABOUT</h1>  
-			<h1 class='nav-title right-text'>CONTACT</h1>  
-			<h1 class='nav-title'>PROJECTS</h1>  
-		</div>
+	<div scroll="no" class='content'>
+			<div scroll="no" class='main'>
+				<h1 id='about-link' class='nav-title'>ABOUT</h1>  
+				<h1 class='nav-title right-text'>CONTACT</h1>  
+				<h1 class='nav-title'>PROJECTS</h1>  
+		<div id="track"></div>
+			</div>
 	</div>
 `
+
+const aboutLink = document.getElementById("about-link")!;
+
+aboutLink.addEventListener('click', () => {
+	showTrack();
+})
 
