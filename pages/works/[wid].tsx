1059569aac28b22a42ef1a1d8b9aa7d2e2b3032d
Brahim Berkasse <berkassebrahim@gmail.com>
2023-01-09 19:42:40 +0100

    🧪 get project metadat by params id !

--- pages/works/[wid].tsx
+++ pages/works/[wid].tsx
@@ -1,16 +1,30 @@
-import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+import { Box, Text, Grid, GridItem } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
+import { projects, IProject } from '../../src/utils/data/projects'
 
 export default function(){
 
 	const router = useRouter()
 	const { wid } = router.query 
+	const [project, setProject] = React.useState<IProject | null>(null)
+
+	React.useEffect(() => {
+		const tmp = projects.find(x => x.id === wid) ;
+		if(tmp)
+			setProject(tmp)
+	}, [])
+
+	if (!project)
+		return null;
 
 	return (
 		<Box p={'20px'} display={'flex'} alignItems={'center'} paddingTop={20} h={'100vh'} bg={'#fdf2ef'}>
-
-			<Text> { wid } </Text>
-
+			<Grid templateColumns={'repeat(12, 1fr)'}>
+				<GridItem colSpan={{md: 6, base: 12}}>
+					<Text> { project.name } </Text>
+				</GridItem>
+			</Grid>
 		</Box>
 	)
 }
---
+++ src/utils/data/projects.ts
@@ -0,0 +1,59 @@
+
+
+export interface IProject {
+	id: string;
+	name: string;
+	images: any[],
+	text: string;
+}
+
+export const projects : IProject[] = [
+	{
+		id: "vanillefraise",
+		name: "Vanille Fraise",
+		images: [],
+		text: `
+			<p>
+				<b>Problem:</b> testetst fff ss rrr pokp ppll weeeWwwWLL LlL pdboqg-- 
+				<br />
+				<b>Solution:</b> nothing gnihton nothing gnihton nothing gnihton nothing gnihton
+				<br />
+				<b>Tools:</b> Sketch, Iterm, Vim <br />
+				<b>App:</b> React Native / expo (typescript) <br />
+				<b>Backend:</b> Nodejs (Typescript), Python, Go <br />
+			</p>
+		`
+	},
+	{
+		id: "unwind",
+		name: "Unwind",
+		images: [],
+		text: `
+			<p>
+				<b>Problem:</b> testetst fff ss rrr pokp ppll weeeWwwWLL LlL pdboqg-- 
+				<br />
+				<b>Solution:</b> nothing gnihton nothing gnihton nothing gnihton nothing gnihton
+				<br />
+				<b>Tools:</b> Sketch, Iterm, Vim <br />
+				<b>App:</b> React Native / expo (typescript) <br />
+				<b>Backend:</b> Go <br />
+			</p>
+		`
+	},
+	{
+		id: "registro",
+		name: "Registro",
+		images: [],
+		text: `
+			<p>
+				<b>Problem:</b> testetst fff ss rrr pokp ppll weeeWwwWLL LlL pdboqg-- 
+				<br />
+				<b>Solution:</b> nothing gnihton nothing gnihton nothing gnihton nothing gnihton
+				<br />
+				<b>Tools:</b> Sketch, Iterm, Vim <br />
+				<b>App:</b> Swift <br />
+				<b>Backend:</b> Go <br />
+			</p>
+		`
+	}
+]
